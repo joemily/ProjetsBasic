76aec88e03c4db7e79e3136c2c563e409e534aaf
joemily campos <joemilybcampos@gmail.com>
2024-08-25 14:31:12 -0300

    Teste de potenciação de um número

--- tj.py
+++ tj.py
@@ -1,3 +1,6 @@
-for i in range(5):
-    print('Teste de função.')
+def teste(valor):
+    for i in range(1, valor + 1):
+        print('{} ** 2 = {}'.format(i, i**2))
 
+resultado = teste(int(input()))
+print(resultado)
